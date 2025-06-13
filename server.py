a68b8c561d0b0c86f8500b19c9ac63d9c05d5b1d
svgen83 <61458549+svgen83@users.noreply.github.com>
2025-06-13 18:47:28 +0400

    скопирован код для стримминга времени, исправлены зависимости

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from aiohttp import web
 import aiofiles
+import datetime
+
+INTERVAL_SECS = 1
+
 
 
 async def archive(request):
@@ -11,11 +15,31 @@ async def handle_index_page(request):
         index_contents = await index_file.read()
     return web.Response(text=index_contents, content_type='text/html')
 
+async def uptime_handler(request):
+    
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
 
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
-        web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archive),
+        #web.get('/', handle_index_page),
+        web.get('/', uptime_handler),
+        #web.get('/archive/{archive_hash}/', archive),
     ])
     web.run_app(app)
