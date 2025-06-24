130777325455782d9a950c1fca1ff034547491f3
svg83 <svgeneraloff@gmail.com>
2025-06-24 10:26:37 +0400

    код для создания архива с применением asyncio

--- async_main.py
+++ async_main.py
@@ -1,16 +1,20 @@
 import asyncio
-import glob
-#import subprocess
 
-async def run():
+
+async def main():
+    archive_bytes = b''
     proc = await asyncio.create_subprocess_shell(
-        'zip -r - photo/*',
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-    
-    archive = await proc.stdout.read()
-    
-    with open('archive.zip', 'wb') as arc:
-        arc.write(archive)
+            'zip -r - \photo',
+            stdout=asyncio.subprocess.PIPE)
+    while not proc.stdout.at_eof():
+        chunk = await proc.stdout.read(102400)
+        archive_bytes += chunk
+        await asyncio.sleep(1)
+    return archive_bytes
 
-asyncio.run(run())
+ 
+archive=asyncio.run(main())
+
+with open('archive.zip', 'wb') as arc:
+            arc.write(archive)
+
--- main.py
+++
@@ -1,9 +0,0 @@
-import glob
-import subprocess
-
-
-with open('photo.zip', 'wb') as archive_file:
-    archive = subprocess.check_output(
-        ['zip', '-r', '-'] + glob.glob('photo/*')
-    )
-    archive_file.write(archive)
